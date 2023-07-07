b6dd28940b649874d4a83d40436f396a7ff5241f
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-07 10:26:58 -0600

    created movies router,controller,service files and set up app.js to have error handlers and use router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const moviesRouter = require("./movies/movies.router")
+
+app.use("/movies", moviesRouter);
+
+// Not found handler
+app.use((req, res, next) => {
+    res.send(`The route ${req.originalUrl} does not exist!`)
+})
+
+// Error handling
+app.use((error, req, res, next) => {
+    const { status = 500, message = `Internal server error`} = error;
+    res.status(status).send(message)
+})
 
 module.exports = app;

bf4479831da4e391f9254bb60616e940547cdb0d
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-07 11:58:44 -0600

    added methodNotAllowed to movies router and updated movies services list() based on query in link

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,7 +1,16 @@
 const knex = require("../db/connection");
 
 function list(){
-    return knex("movies").select("*")
+    const is_showing = req.query.is_showing
+    if( is_showing && is_showing === "true"){
+        return knex("movies")
+        .select("*")
+        .join("movies_theaters", "movies.movie_id", "movies_theaters.movie_id")
+        .join("theaters", "movies_theaters.theater_id", "theaters.theater_id")
+        .where({is_showing: true})
+    }else{
+        return knex("movies").select("*")
+    }
 }
 
 module.exports = {

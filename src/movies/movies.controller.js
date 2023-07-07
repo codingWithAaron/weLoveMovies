d1ba2a1dd200e81e6900c5012ddc99f295e0fc71
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-07 12:33:52 -0600

    updated listIsShowing() in services and controller

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,8 +1,14 @@
 const service = require("./movies.service")
 
 async function list(req, res, next){
-    const data = await service.list()
-    res.json({data})
+    const is_showing = req.query.is_showing
+    if(is_showing && is_showing === "true"){
+        const data = await service.listIsShowing()
+        res.json({data})
+    }else{
+        const data = await service.list()
+        res.json({data})
+    }
 }
 
 module.exports = {
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,18 +1,18 @@
 const knex = require("../db/connection");
 
 function list(){
-    const is_showing = req.query.is_showing
-    if( is_showing && is_showing === "true"){
-        return knex("movies")
+    return knex("movies").select("*")
+}
+
+function listIsShowing(){
+    return knex("movies")
         .select("*")
         .join("movies_theaters", "movies.movie_id", "movies_theaters.movie_id")
         .join("theaters", "movies_theaters.theater_id", "theaters.theater_id")
         .where({is_showing: true})
-    }else{
-        return knex("movies").select("*")
-    }
 }
 
 module.exports = {
-    list
+    list,
+    listIsShowing
 };

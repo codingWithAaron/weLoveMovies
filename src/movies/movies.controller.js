3911cbd710b20080c9e2b11dd23159207d662a68
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-07 10:45:58 -0600

    GET /movies list() added

---
+++ src/movies/movies.controller.js
@@ -0,0 +1,10 @@
+const service = require("./movies.service")
+
+async function list(req, res, next){
+    const data = await service.list()
+    res.json({data})
+}
+
+module.exports = {
+    list
+}
--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("./movies.controller")
 
 router.route("/")
+.get(controller.list)
 
 module.exports = router
